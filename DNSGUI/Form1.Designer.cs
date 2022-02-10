b3a898014ea0098dfaa9040a146303d168fc4e90
Shejan Shuza <shejan0@gmail.com>
2022-02-10 17:41:32 -0600

    Add project files.

---
+++ DNSGUI/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace DNSGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddressInput = new System.Windows.Forms.TextBox();
+            this.IPAddressOutput = new System.Windows.Forms.ListBox();
+            this.PingLabel = new System.Windows.Forms.Label();
+            this.InformationLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AddressInput
+            // 
+            this.AddressInput.Location = new System.Drawing.Point(12, 30);
+            this.AddressInput.Name = "AddressInput";
+            this.AddressInput.Size = new System.Drawing.Size(470, 22);
+            this.AddressInput.TabIndex = 0;
+            this.AddressInput.Text = "google.com";
+            this.AddressInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.AddressInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AddressInput_KeyPressed);
+            // 
+            // IPAddressOutput
+            // 
+            this.IPAddressOutput.FormattingEnabled = true;
+            this.IPAddressOutput.ItemHeight = 16;
+            this.IPAddressOutput.Location = new System.Drawing.Point(12, 75);
+            this.IPAddressOutput.Name = "IPAddressOutput";
+            this.IPAddressOutput.Size = new System.Drawing.Size(470, 228);
+            this.IPAddressOutput.TabIndex = 1;
+            this.IPAddressOutput.SelectedIndexChanged += new System.EventHandler(this.IPAddressOutput_SelectedIndexChanged);
+            // 
+            // PingLabel
+            // 
+            this.PingLabel.AutoSize = true;
+            this.PingLabel.Location = new System.Drawing.Point(13, 418);
+            this.PingLabel.Name = "PingLabel";
+            this.PingLabel.Size = new System.Drawing.Size(0, 17);
+            this.PingLabel.TabIndex = 2;
+            this.PingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InformationLabel
+            // 
+            this.InformationLabel.AutoSize = true;
+            this.InformationLabel.Location = new System.Drawing.Point(13, 347);
+            this.InformationLabel.MaximumSize = new System.Drawing.Size(470, 0);
+            this.InformationLabel.Name = "InformationLabel";
+            this.InformationLabel.Size = new System.Drawing.Size(0, 17);
+            this.InformationLabel.TabIndex = 3;
+            this.InformationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter URL or IP (Hit Enter)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 465);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InformationLabel);
+            this.Controls.Add(this.PingLabel);
+            this.Controls.Add(this.IPAddressOutput);
+            this.Controls.Add(this.AddressInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "DNS GUI Program by Shejan Shuza";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AddressInput;
+        private System.Windows.Forms.ListBox IPAddressOutput;
+        private System.Windows.Forms.Label PingLabel;
+        private System.Windows.Forms.Label InformationLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DNSGUI/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using Newtonsoft.Json;
+
+namespace DNSGUI
+{
+    public partial class Form1 : Form
+    {
+        ListBox.ObjectCollection output;
+        IPAddress selectedIP;
+        bool currentlypinging = false;
+        Ping pingObject = new Ping();
+        PingReply reply;
+        WebClient client = new WebClient();
+        Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            output = IPAddressOutput.Items;
+            DNSSearch();
+            timer = new Timer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = 1;
+           
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            pingDestination();
+        }
+
+        private void AddressInput_KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter || e.KeyChar == (char)Keys.Return)
+            {
+                output.Clear();
+                DNSSearch();
+            }
+        }
+        void DNSSearch()
+        {
+            try
+            {
+                IPAddress[] ipAddresses = Dns.GetHostAddresses(AddressInput.Text);
+                foreach (IPAddress i in ipAddresses)
+                {
+                    output.Add(i);
+                }
+            }catch(Exception e)
+            {
+                output.Add(e.Message);
+            }
+        }
+        void pingDestination()
+        {
+            try
+            {
+                if (!currentlypinging)
+                {
+                    currentlypinging = true;
+                    reply = pingObject.Send(selectedIP);
+                    switch (reply.Status)
+                    {
+                        case IPStatus.Success:
+                            PingLabel.Text = reply.RoundtripTime + "ms to " + selectedIP;
+                            break;
+                        case IPStatus.TimedOut:
+                            PingLabel.Text = reply.Status.ToString();
+                            timer.Enabled = false;
+                            break;
+                        case IPStatus.DestinationNetworkUnreachable:
+                            PingLabel.Text = reply.Status.ToString();
+                            timer.Enabled = false;
+                            break;
+                    }
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        
+                    }
+                    else
+                    {
+                        PingLabel.Text = reply.Status.ToString();
+                    }
+                    currentlypinging = false;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            
+        }
+        void updateInfoforIP()
+        {
+            try
+            {
+                
+                String response = client.DownloadString("http://ip-api.com/json/" + selectedIP.ToString());
+                IP_API_JSON jsonResponse=JsonConvert.DeserializeObject<IP_API_JSON>(response);
+                InformationLabel.Text = jsonResponse.ToString();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+        private void IPAddressOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (IPAddressOutput.SelectedItem is System.Net.IPAddress)
+            {
+                selectedIP = (IPAddress)IPAddressOutput.SelectedItem;
+                updateInfoforIP();
+                timer.Enabled = true;
+
+            }else if (IPAddressOutput.SelectedItem == null)
+            {
+                Console.WriteLine("A Non-existent object was selected");
+                timer.Enabled = false;
+                PingLabel.Text = "";
+            }
+            else
+            {
+                Console.WriteLine("Selected object is not IPAddress, was type: "+IPAddressOutput.SelectedItem.GetType());
+                timer.Enabled = false;
+                PingLabel.Text = "";
+            }
+        }
+
+    }
+    public class IP_API_JSON
+    {
+        //https://ip-api.com/docs/api:json
+        [JsonProperty("query")]
+        public string query { get; set; }
+
+        [JsonProperty("status")]
+        public string status { get; set; }
+
+        [JsonProperty("country")]
+        public string country { get; set; }
+
+        [JsonProperty("countrycode")]
+        public string countryCode { get; set; }
+
+        [JsonProperty("region")]
+        public string region { get; set; }
+        [JsonProperty("regionName")]
+        public string regionName { get; set; }
+        [JsonProperty("city")]
+        public string city { get; set; }
+
+        [JsonProperty("zip")]
+        public string zip { get; set; }
+
+        [JsonProperty("lat")]
+        public string lat { get; set; }
+        [JsonProperty("lon")]
+        public string lon { get; set; }
+        [JsonProperty("timezone")]
+        public string timeZone { get; set; }
+        [JsonProperty("isp")]
+        public string ISP { get; set; }
+        [JsonProperty("org")]
+        public string Org { get; set; }
+        [JsonProperty("as")]
+        public string AS { get; set; }
+
+        public override String ToString()
+        {
+            return "Country: " + country +" ("+countryCode+"), Region: " + regionName + ", City: " + city + ", ZipCode: " + zip + ", Lat,Lon: (" + lat + "," + lon + "), ISP: " + ISP;
+        }
+    }
+  
+}
